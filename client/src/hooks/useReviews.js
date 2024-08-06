05cef2ecbf4b691a9dbf8952cf68c18b2203bbc1
Nikol <nikolche@icloud.com>
2024-08-06 23:09:39 +0300

    Fixed an annoying rendering bug

--- client/src/hooks/useReviews.js
+++ client/src/hooks/useReviews.js
@@ -1,13 +1,6 @@
 import { useEffect, useState } from "react";
 import reviewsAPI from "../api/reviewsAPI";
 
-// export function useCreateReview() {
-//   function createHandler(listingId, review) {
-//     reviewsAPI.createReview(listingId, review);
-//   }
-//   return createHandler;
-// }
-
 export function useGetAllReviews(listingId) {
   const [reviews, setReviews] = useState([]);
 
